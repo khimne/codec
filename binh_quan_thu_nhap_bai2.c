32148faa47780d0a876b2a7938f3b5c016b2e955
khimne <87921186+khimne@users.noreply.github.com>
2021-08-04 20:45:21 +0700

    Update binh_quan_thu_nhap_bai2.c

--- binh_quan_thu_nhap_bai2.c
+++ binh_quan_thu_nhap_bai2.c
@@ -1,21 +1,27 @@
-//IN: năm, GDP , tốc độ tăng  trưởng kinh tế: x
-//OUT: thu nhập bình quân các năm cho đến khi nào GDP năm đó >=2 năm 2014
+/* Cách tính GDP :
+GDP=GDP năm cũ + (GDP năm cũ * tốc độ tăng trưởng) /100
+*/
+//IN: năm GDP, tốc độ tăng trưởng kinh tế: x
 
+//OUT: THU nhập bình quân các năm cho đến khi nào GDP >= 2 năm 2014
+
+
+#include<stdio.h>
 int main()
 {
     int y=0;
     double x,gdp;
-    scanf("%d%lf%lf",&y,&gdp,&x);
-    printf("%10s%10s\n","NAM","GDP");
+    scanf("%d %lf %lf",&y,&gdp,&x);
+    printf("%10s %10s\n","NAM","GDP");
     double k=gdp;
     while(k*2>gdp){
-        printf("%10d%10.2lf\n",y,gdp);
+        printf("%10d %10.2lf\n",y,gdp);
         y++;
         gdp=gdp+gdp*x/100;
         if(gdp>(2*k)){
-            printf("%10d%10.2lf\n", y, gdp);
-            break;
-            }
+            printf("%10d %10.2lf\n",y,gdp);
+        break;
         }
+    }
     return 0;
 }
